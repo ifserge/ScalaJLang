5c5789ce17eed54c33c48a538b4a1e424a659944
sergeif <ifserge@gmail.com>
2014-07-15 23:01:39 +0400

    --init tag

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "ScalaJLang"
+
+version := "0.0.1"
+
+mainClass := Some("ScalaJConsole")
---
+++ src/main/scala/ScalaJLangConsole.scala
@@ -0,0 +1,5 @@
+package ru.sergeif
+
+object ScalaJConsole extends App {
+ println("we are here")
+}
