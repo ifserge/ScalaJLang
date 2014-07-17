c07a67559488463a6d8f9075f738fe68d6375b57
sergeif <ifserge@gmail.com>
2014-07-17 16:19:37 +0400

    + dyad was omplemented

--- src/main/scala/ScalaJLangConsole.scala
+++ src/main/scala/ScalaJLangConsole.scala
@@ -7,14 +7,9 @@ object Shape{
   }
 }
 
-class JMonad(r: Int, mApply: Any => Any){
-  def apply(a: Any): Any =  {
-    val shape = Shape(a)
-    if (shape.length <= r) mApply(a) else a.asInstanceOf[List[_]].map(this.apply(_))
-  }
-}
 object CommonFrame{
   def apply(l: List[Int], r: List[Int]): Option[List[Int]] = (l, r) match {
+    case (Nil, Nil) => Some(Nil)
     case (x::xs, Nil) => Some(Nil)
     case (Nil, y::ys) => Some(Nil)
     case (x::xs,y::ys) => if (x == y) for (t <- CommonFrame(xs, ys)) yield x::t else None
@@ -22,15 +17,41 @@ object CommonFrame{
   }
 }
 
-class JDyad(r: (Int,Int), dApply: (Any,Any) => Any){
+abstract class JToken
+case class JNoun(v: Any, shape: List[Int]) extends JToken
+case class JVerb(s: String) extends JToken
+
+class JMonad(s: String, r: Int, mApply: Any => Any) extends JVerb(s){
+  def apply(a: Any): Any =  {
+    val shape = Shape(a)
+    if (shape.length <= r) mApply(a) else a.asInstanceOf[List[_]].map(apply)
+  }
+}
+class JDyad(s: String, rank: (Int,Int), dApply: (Any,Any) => Any) extends JVerb(s){
   def apply(x: Any, y: Any): Any = {
-    val lshape = Shape(x)
-    val rshape = Shape(y)
-    val lr = r._1
-    val rr = r._2
-    val lf = if (lr != Int.MaxValue) lshape.take(lshape.length - lr) else lshape
-    val rf = if (rr != Int.MaxValue) rshape.take(rshape.length - rr) else rshape
-    val cf =
+    val leftShape = Shape(x)
+    val rightShape = Shape(y)
+    val leftRank = rank._1
+    val rightRank = rank._2
+    val leftFrame = if (leftRank != Int.MaxValue) leftShape.take(leftShape.length - leftRank) else leftShape
+    val rightFrame = if (rightRank != Int.MaxValue) rightShape.take(rightShape.length - rightRank) else rightShape
+    val commonFrameOpt = CommonFrame(leftFrame, rightFrame)
+    if (commonFrameOpt == None)
+      throw new RuntimeException(s"length error in dyad $s")
+    if (commonFrameOpt.getOrElse(Nil) != Nil){
+      val leftArg = x.asInstanceOf[List[Any]]
+      val rightArg = y.asInstanceOf[List[Any]]
+      for {
+        (left,right) <- leftArg zip rightArg
+      } yield apply(left, right)
+    }else{
+      if (leftFrame.length == rightFrame.length && rightFrame.length == 0)
+        dApply(x, y)
+      else if (leftFrame.length > rightFrame.length)
+        x.asInstanceOf[List[Any]].map(apply(_,y))
+      else
+        y.asInstanceOf[List[Any]].map(apply(x,_))
+    }
   }
 }
 
@@ -53,15 +74,16 @@ object ScalaJConsole extends App {
     case (a: BigInt, b: BigInt) => a + b
     case _ => None
   }
-  val incr = new JMonad(0, increment)
-  val inver = new JMonad(Int.MaxValue, reverse)
-  val inver1 = new JMonad(1, reverse)
-  val pluss = new JDyad((0,0), plus)
+  val incr = new JMonad(">:", 0, increment)
+  val inver = new JMonad("|.", Int.MaxValue, reverse)
+  val inver1 = new JMonad("|.\"1", 1, reverse)
+  val pluss = new JDyad("+", (0,0), plus)
   println(incr(3))
   println(inver(List(List(5,6))))
   println(inver1(List(List(5,6), List(1,2,3))))
   println(pluss(1,2))
   println(pluss(List(1,2,3),2))
-  println(pluss(List(1,2,3),List(2,3)))
+  //println(pluss(List(1,2,3),List(2,3)))
   println(pluss(List(1,2,3),List(-1,-1,-1)))
+  println(pluss(List(List(1,2,3), List(3,4,5)),List(List(-1,-1,-1),List(-5,-5,-5))))
 }
